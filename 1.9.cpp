ced1c5d5bb2e94cdf356e0961a7073e37acd4071
Infinioka <142238574+infiniokas@users.noreply.github.com>
2024-10-10 08:59:45 +0300

    Update 1.9.cpp

--- 1.9.cpp
+++ 1.9.cpp
@@ -1,51 +1,39 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <math.h>
 
-double calculate_value(double x, double z) {
-    if (z <= 0 || (1 + pow(x, 4)) == 0) {
-        printf("Ошибка: деление на ноль или логарифм от отрицательного числа\n");
-        return 0; // Возвращаем NaN для обозначения ошибки
-    }
-    return (sqrt(1 + z * z) + abs(pow(log(z), 3)) / (1 + pow(x, 4))) * sin(7 * x);
-}
-int main()
-{ //setlocale(LC_ALL, "Russian");
+int main() {
+    int N, M;
     double Xstart, Xstop, Zstart, Zstop, HX, HZ;
-    double x, z, y;
-    printf("Введите значение Xstart ");
+
+    printf("Введите N (количество значений по оси X): ");
+    scanf("%d", &N);
+    printf("Введите M (количество значений по оси Z): ");
+    scanf("%d", &M);
+
+    printf("Введите Xstart: ");
     scanf("%lf", &Xstart);
-    printf("Введите значение Xstop ");
+    printf("Введите Xstop: ");
     scanf("%lf", &Xstop);
-    printf("Введите значение Zstart ");
+    printf("Введите Zstart: ");
     scanf("%lf", &Zstart);
-    printf("Введите значение Zstop ");
+    printf("Введите Zstop: ");
     scanf("%lf", &Zstop);
-    printf("Введите значение HX ");
-    scanf("%lf", &HX);
-    printf("Введите значение HZ ");
-    scanf("%lf", &HZ);
-    if (Xstop <= Xstart || Zstop <= Zstart || HX <= 0 || HZ <= 0) {
-        printf("Некорректный ввод данных\n");
-        return 1;
-    }
 
-    printf("Таблица значений по заданной формуле\n");
-    printf("%6s |", "x");
-    for (z = Zstart; z <= Zstop; z += HZ) {
-        printf("%10.2lf|", z);
-    }
-    printf("\n");
+    HX = (Xstop - Xstart) / (N - 1);
+    HZ = (Zstop - Zstart) / (M - 1);
 
-    for (x = Xstart; x <= Xstop; x += HX) {
-        printf("%6.2f |", x);
-        for (z = Zstart; z <= Zstop; z += HZ) {
-            double sqrt_z_sq = sqrt(1 + z * z); // Вычисление sqrt(1 + z*z) один раз
-            double value = calculate_value(x, z);
-            if (isnan(value)) {
-                printf("  Ошибка  |");
-            }
-            else {
-                printf("%10.5lf|", value);
+    for (int i = 0; i < N; i++) {
+        double x = Xstart + i * HX;
+        printf("При x = %.2lf:\n", x);
+        for (int j = 0; j < M; j++) {
+            double z = Zstart + j * HZ;
+            if (z <= 0 || 1 + pow(x, 4) == 0) {
+                printf("  Ошибка: деление на ноль или логарифм от отрицательного числа\n");
+            } else {
+                double y1 = sqrt(1 + z * z);
+                double y2 = abs(pow(log(z), 3)) / (1 + pow(x, 4));
+                double y = (y1 + y2) * sin(7 * x);
+                printf("  При z = %.2lf, y = %.5lf\n", z, y);
             }
         }
         printf("\n");
