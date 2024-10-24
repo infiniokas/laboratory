5ff937760d8272c415af2a717477cb7334a90844
Infinioka <142238574+infiniokas@users.noreply.github.com>
2024-10-24 17:15:14 +0300

    Update 3.1.cpp

--- 3.1.cpp
+++ 3.1.cpp
@@ -25,3 +25,51 @@ int main() {
 
     return 0;
 }
+
+#include<iostream>
+#include<math.h>
+#include<stdio.h>
+
+int main() {
+    int n,i,m;
+    int  g;
+    bool flag;
+    
+    printf("Введите количество элементов в массиве ");
+    scanf("%i", &n);
+    if (n<=0 || n>20){
+        while (n<=0 || n>20){
+            printf("Введено неверное количество\n");
+            printf("Введите новое количество элементов в массиве ");
+            scanf("%i", &n);
+        }
+    }
+
+
+    float *A;
+    A=new float[n];
+    printf("Введите элементы массива:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%f", &A[i]);
+    }
+    printf("Введенные элементы массива:");
+    for (int i = 0; i < n; i++) {
+        printf(" %f ", A[i]);
+    }
+    
+    flag = true; // Флаг для отслеживания сортировки
+    i=0;
+    while (i<n){
+        if (A[i] < A[i-1]) {
+            flag=false;
+            g=i;
+        } i++;}
+    if (!flag) {
+        printf("\nМассив не отсортирован\n");
+        printf("На позиции %d нарушение: %f\n", g, A[g]);
+        }
+
+    else printf("\n Массив отсортирован по возрастанию.\n");
+    
+    return 0;
+}
